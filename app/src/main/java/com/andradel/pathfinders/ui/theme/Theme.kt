57faa98cae51638355ca7e58d75f9c7ece642cd2
Luis Andrade <luis.andrade@getbux.com>
2023-09-27 19:37:35 +0100

    Remove comments

--- app/src/main/java/com/andradel/pathfinders/ui/theme/Theme.kt
+++ app/src/main/java/com/andradel/pathfinders/ui/theme/Theme.kt
@@ -26,15 +26,6 @@ private val lightColorPalette = lightColorScheme(
     primary = red,
     primaryContainer = lightYellow,
     secondary = yellow,
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
