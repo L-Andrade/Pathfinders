e59e2955e474c490885bdb59073d44ead788aad0
Luis Andrade <luis.andrade@getbux.com>
2025-08-12 18:10:39 +0100

    : Remove Firebase DI providers
    They do not seem compatible due to expect/actual, so we can't inject them through constructors

--- shared/src/commonMain/kotlin/com/andradel/pathfinders/shared/firebase/FirebaseModule.kt
+++ shared/src/commonMain/kotlin/com/andradel/pathfinders/shared/firebase/FirebaseModule.kt
@@ -5,14 +5,4 @@ import org.koin.core.annotation.Module
 
 @Module
 @ComponentScan("com.andradel.pathfinders.firebase**")
-class FirebaseModule {
-    // TODO
-//     @Single
-//     fun providesFirebaseAuth(): FirebaseAuth = Firebase.auth
-//
-//     @Single
-//     fun providesFirebaseDb(): FirebaseDatabase = Firebase.database
-//
-//     @Single
-//     fun providesFirebaseFunctions(): FirebaseFunctions = Firebase.functions
-}
+class FirebaseModule
