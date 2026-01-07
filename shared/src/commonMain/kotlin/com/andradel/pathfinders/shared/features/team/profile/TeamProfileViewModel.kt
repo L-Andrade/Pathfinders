950d2251f33a027fdd2398a8e9089f4b1d267c04
Luis Andrade <luis.andrade@getbux.com>
2026-01-07 17:43:00 +0000

    Indentation and sort by name in ascending order

--- shared/src/commonMain/kotlin/com/andradel/pathfinders/shared/features/team/profile/TeamProfileViewModel.kt
+++ shared/src/commonMain/kotlin/com/andradel/pathfinders/shared/features/team/profile/TeamProfileViewModel.kt
@@ -22,49 +22,48 @@ class TeamProfileViewModel(
     activityDataSource: ActivityFirebaseDataSource,
 ) : ViewModel() {
 
-    val state: StateFlow<TeamProfileState> =
-        combine(
-            dataSource.team(team.id, archiveName),
-            activityDataSource.activitiesForTeam(archiveName, team.id),
-        ) { team, activities ->
-            val team = team ?: this.team
-            TeamProfileState.Loaded(
-                title = team.name,
-                teamParticipants = team.participants.map { participant ->
-                    val participantActivities = activities
-                        .filter { it.teamScores[team.id]?.containsKey(participant.id) == true }
-                        .map {
-                            TeamParticipantActivity(
-                                id = it.id,
-                                name = it.name,
-                                points = it.teamPointsForParticipant(team.id, participant.id),
-                            )
-                        }
-                    TeamParticipant(
-                        id = participant.id,
-                        name = participant.name,
-                        points = participantActivities.sumOf { it.points },
-                        activities = participantActivities,
-                    )
-                }.sortedWith(compareBy({ -it.activities.size }, { -it.points })),
-                teamActivities = activities.sortedByDescending { it.date }.map { activity ->
-                    val participantIds = activity.teamScores[team.id]?.keys.orEmpty()
-                    TeamActivity(
-                        id = activity.id,
-                        name = activity.name,
-                        date = activity.date?.toString(),
-                        participants = participantIds.map { id ->
-                            val participant = team.participants.find { it.id == id }
-                            TeamActivityParticipant(
-                                id = id,
-                                name = participant?.name,
-                                points = activity.teamPointsForParticipant(team.id, id),
-                            )
-                        }.sortedByDescending { it.name },
-                        total = activity.teamPoints(team.id),
-                    )
-                },
-                points = activities.sumOf { it.teamPoints(this.team.id) },
-            )
-        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), TeamProfileState.Loading(team.name))
+    val state: StateFlow<TeamProfileState> = combine(
+        dataSource.team(team.id, archiveName),
+        activityDataSource.activitiesForTeam(archiveName, team.id),
+    ) { team, activities ->
+        val team = team ?: this.team
+        TeamProfileState.Loaded(
+            title = team.name,
+            teamParticipants = team.participants.map { participant ->
+                val participantActivities = activities
+                    .filter { it.teamScores[team.id]?.containsKey(participant.id) == true }
+                    .map {
+                        TeamParticipantActivity(
+                            id = it.id,
+                            name = it.name,
+                            points = it.teamPointsForParticipant(team.id, participant.id),
+                        )
+                    }
+                TeamParticipant(
+                    id = participant.id,
+                    name = participant.name,
+                    points = participantActivities.sumOf { it.points },
+                    activities = participantActivities,
+                )
+            }.sortedWith(compareBy({ -it.activities.size }, { -it.points })),
+            teamActivities = activities.sortedByDescending { it.date }.map { activity ->
+                val participantIds = activity.teamScores[team.id]?.keys.orEmpty()
+                TeamActivity(
+                    id = activity.id,
+                    name = activity.name,
+                    date = activity.date?.toString(),
+                    participants = participantIds.map { id ->
+                        val participant = team.participants.find { it.id == id }
+                        TeamActivityParticipant(
+                            id = id,
+                            name = participant?.name,
+                            points = activity.teamPointsForParticipant(team.id, id),
+                        )
+                    }.sortedBy { it.name },
+                    total = activity.teamPoints(team.id),
+                )
+            },
+            points = activities.sumOf { it.teamPoints(this.team.id) },
+        )
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), TeamProfileState.Loading(team.name))
 }
